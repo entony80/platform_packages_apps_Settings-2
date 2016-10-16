ad70f5d637fcf0ce8019321ed60c81b9ad427a32
starkdroid <gamerprince.exp@gmail.com>
2016-10-16 19:00:37 +0530

    Versioning
    Conflicts:
    	res/values/zephyr_strings.xml
    	res/xml/device_info_settings.xml

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -163,6 +163,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         }
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_ZEPHYR_VERSION, "ro.zephyr.version");
+        findPreference(KEY_ZEPHYR_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
