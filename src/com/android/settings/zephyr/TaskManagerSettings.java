d807353a083cb9a452ee3b1da8b69558caf116bd
starkdroid <gamerprince.exp@gmail.com>
2017-02-03 12:35:41 +0530

    Fix TaskManager FC

--- src/com/android/settings/zephyr/TaskManagerSettings.java
+++ src/com/android/settings/zephyr/TaskManagerSettings.java
@@ -11,7 +11,7 @@
      See the License for the specific language governing permissions and
      limitations under the License.
 */
-package com.android.settings.rr;
+package com.android.settings.zephyr;
 
 import android.app.Activity;
 import android.content.Context;
